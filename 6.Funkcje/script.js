5cd807c7eef436d0f3212d8642c891c474a740b6
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-19 16:21:24 +0200

    Update script.js

---
+++ 6.Funkcje/script.js
@@ -0,0 +1,23 @@
+//Zadanie 1
+
+function x(){console.log("Udało Się!")}
+x();
+
+//Zadanie 2
+
+function x(z){console.log(z)}
+
+x(2137);
+x("XD");
+x('!');
+x(NaN);
+x(undefined);
+
+//Zadanie 3
+
+function x(y){return y}
+const tab = [2, 13, "XD", '7']
+x(tab);
+
+//Zadanie 4
+
