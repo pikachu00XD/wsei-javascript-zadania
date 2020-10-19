5c5e13ae2160aff8a2fa1257535e73f53fd1327f
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-19 14:57:02 +0200

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,19 @@
+// Zadanie 1
+
+console.log('Maks');
+
+// Zadanie 2
+
+console.log(12+32);
+console.log(15/16);
+console.log(5*2);
+
+// Zadanie 3
+
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World);
+
+// w punkcie 1 jest string
+// w punkcie 2 szuka zmiennej
+// w punkcie 3 jest niedomknięty cudzysłów 
