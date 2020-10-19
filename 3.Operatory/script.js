23450242543c4b0a3cad3ef95486926acbcd4de9
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-19 14:59:57 +0200

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,60 @@
+//Zadanie 1
+
+var value1 = true;
+var value2 = false;
+
+console.log(value1 === value2);
+
+//Zadanie 2
+
+var number1 = 5;
+var number2 = 2;
+
+var moduloResult = 0;
+
+moduloResult = number1 % number2;
+
+console.log(moduloResult);
+
+//Zadanie 3
+
+let word1 = "JP";
+let word2 = "2GMD";
+
+let stringResult = "";
+
+stringResult = word1 + word2;
+
+console.log(stringResult);
+
+//Zadanie 4
+
+var someNumber =425;
+var someString ="425";
+
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+
+//Zadanie 5
+
+var counter = 30;
+
+counter ++;
+
+console.log(counter);
+
+counter --;
+
+console.log(counter);
+
+
+//Zadanie 6
+
+var number3 = 4;
+var number4 = 5;
+var result = null;
+
+result = number3 > number4;
+
+console.log(result);
