d6c9fa7d967e53182046e9ccede73401fd2e6973
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-19 17:39:44 +0200

    Update script.js

--- 7. Tablice/script.js
+++ 7. Tablice/script.js
@@ -9,3 +9,25 @@ for(var i=0; i<3; i++)
   
 //Zadanie 2
 
+var tab = [2137, "XD", 420, 69, "jp2gmd", "papaj"];
+
+console.log(tab[0] + tab[1]);
+console.log(tab[tab.length - 1]);
+console.log(tab);
+
+for (var i=0; i<tab.length; i+=2)
+  {
+    console.log(tab[i]);
+  }
+
+var filteredStr = tab.filter(function(value)
+{
+  return!(parseInt(value) == value)
+})
+console.log(filteredStr);
+
+var filteredNum = tab.filter(function(value)
+{
+  return(parseInt(value) == value)
+})
+console.log(filteredNum);
