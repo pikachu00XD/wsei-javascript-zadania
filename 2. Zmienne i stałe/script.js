9bf4cf5505a81f35a2b557fd8b1607e141d5bb9e
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-19 15:01:21 +0200

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,30 @@
+//Zadanie 1
+
+var num = 2137;
+var str = "jp2gmd";
+var sum = 21 + "trzysiedem";
+var log = true;
+var spec = null;
+
+console.log(num);
+console.log(str);
+console.log(sum);
+console.log(log);
+console.log(spec);
+
+//Zadanie 2
+
+var num1 = 21;
+var num2 = 37;
+var sum = 0;
+
+console.log(num1+num2);
+sum = num1+num2;
+
+//Zadanie 3
+
+var nothin;
+console.log(nothin);
+
+//Wyskakuje wartość undefined oznaczająca, że zmienna została stworzona ale żadna wartość, a co za tym rodzaj zmiennej nie został w niej zdefiniowany, zajmuje natomiast ona miejsce w pamięci na wypadek gdybyśmy chcieli coś do niej przypisać
+
