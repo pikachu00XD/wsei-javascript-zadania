c1551976e3c8126ebc843f7861ea29ed5c206949
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-22 11:03:33 +0200

    Update script.js

--- 8.Obiekty/script.js
+++ 8.Obiekty/script.js
@@ -10,3 +10,17 @@ const car = {
 console.log(car);
 
 //Zadanie 2
+
+const car = {
+  name : "BMW",
+  color : "red",
+  type : "sport",
+  showText() {return "bruuum";},
+  changeName : function(name)
+  {
+    this.name = name;
+  }
+}
+car.changeName("Mercedes");
+
+//Zadanie 3
