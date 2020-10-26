fceb26e4bb61089b41cfeda9d2cb7784648be717
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-10-26 13:46:12 +0100

    Update script.js

--- 10.This/script.js
+++ 10.This/script.js
@@ -17,9 +17,7 @@ Nazwisko: ${this.lastName}\n
 Wiek: ${this.age}\n 
 Państwo: ${this.country}\n 
 Miasto: ${this.city}`);
-
 }
-
 Person.prototype.upAge = function()
 {
   this.age += 1;
@@ -38,3 +36,42 @@ person2.upAge();
 person2.info();
 
 //Zadanie2
+
+//Zadanie 1
+
+function Person(firstName, lastName, age, country, city)
+{
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.country = country;
+  this.city = city
+}
+
+Person.prototype.info = function()
+{
+    console.log(`
+Imie: ${this.firstName}\n 
+Nazwisko: ${this.lastName}\n 
+Wiek: ${this.age}\n 
+Państwo: ${this.country}\n 
+Miasto: ${this.city}`);
+}
+Person.prototype.upAge = function()
+{
+  this.age += 1;
+}
+
+let person = new Person("Adam", "Nowak", 19, "Poland", "Warsaw");
+let person2 = new Person("Grzegorz", "Brzęczyszczykiewicz", 25, "Poland", "Chrząszczyrzewoszyce");
+
+Person.favFood = [];
+Person.prototype.addFood = function(food)
+{
+  if(this.favFood === undefined) this.favFood = `${food}, `;
+  else this.favFood += `${food}, `;
+}
+Person.prototype.vievFood = function()
+{
+  console.log(this.favFood);
+}
