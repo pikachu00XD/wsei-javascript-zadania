d7ab958a6a79ba056abec7d88abb9234a46bc598
pikachu00XD <62176387+pikachu00XD@users.noreply.github.com>
2020-11-01 20:25:47 +0100

    Update script.js

--- 2. Zmienne i stałe/script.js
+++ 2. Zmienne i stałe/script.js
@@ -28,3 +28,7 @@ console.log(nothin);
 
 //Wyskakuje wartość undefined oznaczająca, że zmienna została stworzona ale żadna wartość, a co za tym rodzaj zmiennej nie został w niej zdefiniowany, zajmuje natomiast ona miejsce w pamięci na wypadek gdybyśmy chcieli coś do niej przypisać
 
+//           _
+//       .__(.)< *kwa*Ten kod nie nadaje się
+//        \___)   do dializy nerek*kwa*
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
